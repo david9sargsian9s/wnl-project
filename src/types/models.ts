a92863cc08adc7f64e3a80cb22b8cfd846f2c21d
david9sargsian9s <david9sargsyan9s@gmail.com>
2026-07-13 16:50:47 +0400

    fix: resolve typescript import registry error for models

--- src/types/models.ts
+++ src/types/models.ts
@@ -1,9 +1,9 @@
 import { UserModel } from "../model/userModel";
-import { productModel } from "../model/EventModel";
+import { EventModel } from "../model/EventModel";
 import { tokenModel } from "../model/tokenModel";
 
 export interface IModels {
     users: typeof UserModel;
-    products: typeof productModel;
+    products: typeof EventModel;
     token: typeof tokenModel;
 }
